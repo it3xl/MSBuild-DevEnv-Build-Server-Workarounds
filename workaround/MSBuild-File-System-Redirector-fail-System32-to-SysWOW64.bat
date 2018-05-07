4521b394570d9f9569b6e211e3a2ec4d2f11a532
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-05-07 19:16:08 +0300

    Reformats everything in a more concise fashion.

--- workaround/MSBuild-File-System-Redirector-fail-System32-to-SysWOW64.bat
+++ workaround/MSBuild-File-System-Redirector-fail-System32-to-SysWOW64.bat
@@ -1,58 +1,60 @@
-@SET operation=MSBuild: Preventing File System Redirector fail.
-@ECHO START: %operation%
-  @REM This workaround is actual if your run under the Local System account.
-  @REM
-  @REM For any build under the system local account (Local System for our Build-Server's Win Service) there is a bug for implicit switching
-  @REM  from C:\Windows\System32\config\systemprofile to C:\Windows\SysWOW64\config\systemprofile\AppData
-  @REM A workaround for this is to use x64 MSBuild but we have no this option for the DevEnv calling.
-  @REM
-  @REM It is important to modify the CMD environment by this workaround before call of the MSBuild or DevEnv Command Prompt.
-  @REM VsDevCmd.bat will install some .NET stuff into a current AppData.
-  @REM And possibly DevEnv will be using of C:\Windows\system32\... again for the Local System account.
-  @REM URLs:
-  @REM https://social.msdn.microsoft.com/Forums/en-US/70f80ccd-9e15-49db-94ea-4564dfc1e593/msbuild-failure-what-permissions-are-needed-to-build-workflow-projects-compileworkflowtask?forum=msbuild
-  @REM https://msdn.microsoft.com/en-us/library/windows/desktop/aa384187%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
-  @REM http://stackoverflow.com/questions/9096208/cannot-build-workflow-project-with-msbuild-sharepoint-2010/9097079
-  @REM https://connect.microsoft.com/VisualStudio/feedback/details/1580569/32-bit-msbuild-exe-running-as-local-system-calls-64-bit-vbcscompiler-exe-causing-csc-error-cs2001-source-file-file-could-not-be-found
-  @REM
-  @REM Allows to eliminate any use of the x64 MSBuild that was requested from the URLs above.
-  
-  @REM @SET homeRootRelative=Jenkins-Home
-  @REM @SET homeRootRelative=CCN-Home
-  @SET homeRootRelative=Build-Server-Home
+ECHO Start %~nx0
 
-  @ECHO %HOMEPATH% switched to
-  @SET HOMEPATH=\%homeRootRelative%
-  @ECHO %HOMEPATH%
-  
-  @SET homeRootAbsolute=C:\%homeRootRelative%
 
-  @ECHO %APPDATA% switched to
-  @SET APPDATA=%homeRootAbsolute%\AppData\Roaming
-  @ECHO %APPDATA%
+@REM This workaround is actual if your run under the Local System account.
+@REM
+@REM For any build under the system local account (Local System for our Build-Server's Win Service) there is a bug for implicit switching
+@REM  from C:\Windows\System32\config\systemprofile to C:\Windows\SysWOW64\config\systemprofile\AppData
+@REM A workaround for this is to use x64 MSBuild but we have no this option for the DevEnv calling.
+@REM
+@REM It is important to modify the CMD environment by this workaround before call of the MSBuild or DevEnv Command Prompt.
+@REM VsDevCmd.bat will install some .NET stuff into a current AppData.
+@REM And possibly DevEnv will be using of C:\Windows\system32\... again for the Local System account.
+@REM URLs:
+@REM https://social.msdn.microsoft.com/Forums/en-US/70f80ccd-9e15-49db-94ea-4564dfc1e593/msbuild-failure-what-permissions-are-needed-to-build-workflow-projects-compileworkflowtask?forum=msbuild
+@REM https://msdn.microsoft.com/en-us/library/windows/desktop/aa384187%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
+@REM http://stackoverflow.com/questions/9096208/cannot-build-workflow-project-with-msbuild-sharepoint-2010/9097079
+@REM https://connect.microsoft.com/VisualStudio/feedback/details/1580569/32-bit-msbuild-exe-running-as-local-system-calls-64-bit-vbcscompiler-exe-causing-csc-error-cs2001-source-file-file-could-not-be-found
+@REM
+@REM Allows to eliminate any use of the x64 MSBuild that was requested from the URLs above.
 
-  @ECHO %LOCALAPPDATA% switched to
-  @SET LOCALAPPDATA=%homeRootAbsolute%\AppData\Local
-  @ECHO %LOCALAPPDATA%
+@REM @SET homeRootRelative=Jenkins-Home
+@REM @SET homeRootRelative=CCN-Home
+@SET homeRootRelative=Build-Server-Home
 
-  @ECHO %TEMP% switched to
-  @SET TEMP=%homeRootAbsolute%\TEMP
-  @ECHO %TEMP%
+@ECHO %HOMEPATH% switched to
+@SET HOMEPATH=\%homeRootRelative%
+@ECHO %HOMEPATH%
 
-  @ECHO %TMP% switched to
-  @SET TMP=%homeRootAbsolute%\TEMP
-  @ECHO %TMP%
+@SET homeRootAbsolute=C:\%homeRootRelative%
 
-  @ECHO %USERPROFILE% switched to
-  @SET USERPROFILE=%homeRootAbsolute%
-  @ECHO %USERPROFILE%
-  
-  
-  @REM The \TEMP folder must exist otherwise there will be errors. Nothing can not create it. Mad!
-  @IF NOT EXIST %homeRootAbsolute%\TEMP (MKDIR %homeRootAbsolute%\TEMP)
-  @REM The \AppData\Local\Temp folder must exist otherwise there will be errors. Nothing can not create it. Mad!
-  @IF NOT EXIST %LOCALAPPDATA%\Temp (MKDIR %LOCALAPPDATA%\Temp)
-  
-  @CALL %env_qUtil%\exit_if_error
-  
-@ECHO DONE: %operation%
+@ECHO %APPDATA% switched to
+@SET APPDATA=%homeRootAbsolute%\AppData\Roaming
+@ECHO %APPDATA%
+
+@ECHO %LOCALAPPDATA% switched to
+@SET LOCALAPPDATA=%homeRootAbsolute%\AppData\Local
+@ECHO %LOCALAPPDATA%
+
+@ECHO %TEMP% switched to
+@SET TEMP=%homeRootAbsolute%\TEMP
+@ECHO %TEMP%
+
+@ECHO %TMP% switched to
+@SET TMP=%homeRootAbsolute%\TEMP
+@ECHO %TMP%
+
+@ECHO %USERPROFILE% switched to
+@SET USERPROFILE=%homeRootAbsolute%
+@ECHO %USERPROFILE%
+
+
+@REM The \TEMP folder must exist otherwise there will be errors. Nothing can not create it. Mad!
+@IF NOT EXIST %homeRootAbsolute%\TEMP (MKDIR %homeRootAbsolute%\TEMP)
+@REM The \AppData\Local\Temp folder must exist otherwise there will be errors. Nothing can not create it. Mad!
+@IF NOT EXIST %LOCALAPPDATA%\Temp (MKDIR %LOCALAPPDATA%\Temp)
+
+@CALL %env_qUtil%\exit_if_error
+
+
+ECHO End %~nx0
--- workaround/MSBuild-Unable-to-create-Temp-directory.bat
+++ workaround/MSBuild-Unable-to-create-Temp-directory.bat
@@ -1,18 +1,20 @@
-@SET operation=MSBuild: Preventing of a bug with a non existing Temp folder.
-@ECHO START: %operation%
-  @REM This workaround is actual fot those who uses the "CompileWorkflowTask".
-  @REM
-  @REM Some MSBuild tasks really can't create a C:\...\AppData\Local\Temp folder and throws the following error:
-  @REM warning MSB3245: Could not resolve this reference. Could not locate the assembly "WF.WorkflowCore, Version=27.0.5088.24788, Culture=neutral, processorArchitecture=MSIL".
-  @REM Check to make sure the assembly exists on disk. If this reference is required by your code, you may get compilation errors.
-  @REM error MSB4018: The "CompileWorkflowTask" task failed unexpectedly.
-  @REM C:\Windows\Microsoft.NET\Framework\v4.0.30319\Workflow.targets(121,5): error MSB4018: System.IO.DirectoryNotFoundException: Could not find a part of the path 'C:\...\AppData\Local\Temp\???.tmp'.
-  @REM URLs:
-  @REM http://stackoverflow.com/questions/9096208/cannot-build-workflow-project-with-msbuild-sharepoint-2010/9097079
-  @REM https://social.msdn.microsoft.com/Forums/en-US/70f80ccd-9e15-49db-94ea-4564dfc1e593/msbuild-failure-what-permissions-are-needed-to-build-workflow-projects-compileworkflowtask?forum=msbuild
-  
-  @IF NOT EXIST %LOCALAPPDATA%\Temp (MKDIR %LOCALAPPDATA%\Temp)
+ECHO Start %~nx0
 
-  @CALL %env_qUtil%\exit_if_error
 
-@ECHO END: %operation%
+@REM This workaround is actual fot those who uses the "CompileWorkflowTask".
+@REM
+@REM Some MSBuild tasks really can't create a C:\...\AppData\Local\Temp folder and throws the following error:
+@REM warning MSB3245: Could not resolve this reference. Could not locate the assembly "WF.WorkflowCore, Version=27.0.5088.24788, Culture=neutral, processorArchitecture=MSIL".
+@REM Check to make sure the assembly exists on disk. If this reference is required by your code, you may get compilation errors.
+@REM error MSB4018: The "CompileWorkflowTask" task failed unexpectedly.
+@REM C:\Windows\Microsoft.NET\Framework\v4.0.30319\Workflow.targets(121,5): error MSB4018: System.IO.DirectoryNotFoundException: Could not find a part of the path 'C:\...\AppData\Local\Temp\???.tmp'.
+@REM URLs:
+@REM http://stackoverflow.com/questions/9096208/cannot-build-workflow-project-with-msbuild-sharepoint-2010/9097079
+@REM https://social.msdn.microsoft.com/Forums/en-US/70f80ccd-9e15-49db-94ea-4564dfc1e593/msbuild-failure-what-permissions-are-needed-to-build-workflow-projects-compileworkflowtask?forum=msbuild
+
+@IF NOT EXIST %LOCALAPPDATA%\Temp (MKDIR %LOCALAPPDATA%\Temp)
+
+@CALL %env_qUtil%\exit_if_error
+
+
+ECHO End %~nx0
