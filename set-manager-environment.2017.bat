c4973c0ff5b227bf9048daad8058a06dbde9a114
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2020-02-13 12:29:40 +0300

    Typo. Not tested yet.

--- set-manager-environment.2017.bat
+++ set-manager-environment.2017.bat
@@ -13,7 +13,7 @@ SET __env_all_vs2017_root=%ProgramFiles(x86)%\Microsoft Visual Studio\2017
 SET env_vs2017_install="%__env_all_vs2017_root%\Professional"
 IF NOT EXIST %env_vs2017_install%  SET env_vs2017_install="%__env_all_vs2017_root%\Community"
 IF NOT EXIST %env_vs2017_install%  SET env_vs2017_install="%__env_all_vs2017_root%\Enterprise"
-@REM Let's fail laudly
+@REM Let's fail loudly
 IF NOT EXIST %env_vs2017_install%  SET "env_vs2017_install=Visual Studio 2017 install path was not found in %~nx0"
 
 SET env_vs2017_dev_cmd="%env_vs2017_install:"=%\Common7\Tools\VsDevCmd.bat"
--- set-manager-environment.2019.bat
+++ set-manager-environment.2019.bat
@@ -13,7 +13,7 @@ SET __env_all_vs2019_root=%ProgramFiles(x86)%\Microsoft Visual Studio\2019
 SET env_vs2019_install="%__env_all_vs2019_root%\Professional"
 IF NOT EXIST %env_vs2019_install%  SET env_vs2019_install="%__env_all_vs2019_root%\Community"
 IF NOT EXIST %env_vs2019_install%  SET env_vs2019_install="%__env_all_vs2019_root%\Enterprise"
-@REM Let's fail laudly
+@REM Let's fail loudly
 IF NOT EXIST %env_vs2019_install%  SET "env_vs2019_install=Visual Studio 2019 install path was not found in %~nx0"
 
 SET env_vs2019_dev_cmd="%env_vs2019_install:"=%\Common7\Tools\VsDevCmd.bat"
